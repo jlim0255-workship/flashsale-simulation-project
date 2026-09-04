cc13a52352a0ab5447523f3598b99e9edcdaed66
jlim0255-workship <jlim0255.work@gmail.com>
2026-09-04 21:30:59 +0800

    MEAT: implemented purchase in service layer,  if repo successfully decrease the ticket count, update the orders table

--- src/main/java/com/jlim/flashsale_simulation/service/PurchaseService.java
+++ src/main/java/com/jlim/flashsale_simulation/service/PurchaseService.java
@@ -1,4 +1,43 @@
 package com.jlim.flashsale_simulation.service;
 
+import com.jlim.flashsale_simulation.dto.PurchaseResult;
+import com.jlim.flashsale_simulation.exception.SoldOutException;
+import com.jlim.flashsale_simulation.strategy.InventoryStrategy;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+@Service
 public class PurchaseService {
+
+    private final InventoryStrategy strategy;
+    private final JdbcTemplate jdbcTemplate;
+
+    public PurchaseService(InventoryStrategy strategy, JdbcTemplate jdbcTemplate) {
+        this.strategy = strategy;
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Transactional
+    public PurchaseResult purchase (long eventId, String userId) {
+        /*
+        * if (!strategy.tryDecrement(eventId)) throw new SoldOutException();
+        * UUID orderId = UUID.randomUUID();
+        * jdbcTemplate.update("INSERT INTO orders (id, event_id, user_id, idempotency_key, status) VALUES (?, ?, ?, ?, ?)", orderId, eventId, userId, null, "CONFIRMED");
+        * return new PurchaseResult(orderId, "CONFIRMED");
+        * */
+
+        // throw exception to rollback if tryDecrement return false
+        if (!strategy.tryDecrement(eventId)){
+            throw new SoldOutException("Tickets Sold Out!!!");
+        }
+
+        UUID orderId = UUID.randomUUID();
+
+        jdbcTemplate.update("INSERT INTO orders (id, event_id, user_id, idempotency_key, status) VALUES (?, ?, ?, ?, ?)", orderId, eventId, userId, null, "CONFIRMED");
+
+        return new PurchaseResult(orderId, "CONFIRMED");
+    }
 }
